a9d3437fa28142a7a9882bc8e233552e857a848e
Clarence Bumanglag <c.bumanglag.15@gmail.com>
2019-02-26 17:56:26 -0600

    Added DateValidator Class

---
+++ DateValidator.java
@@ -0,0 +1,29 @@
+package meatbol;
+
+public class DateValidator {
+
+    private static final int TOTAL_MONTHS = 12;
+    private static final int[] DAYS31 = {1,3,5,7,8,10,12};
+    private int index = 0;
+    private static final String NUMBERS = "1234567890";
+
+    private String year = null;
+    private String month = null;
+    private String day = null;
+
+    public DateValidator() { }
+
+    public boolean validateDate(String strTok) {
+        String str = null;
+
+        if (strTok.length() != 10) {
+            return false;
+        }
+
+        for (int i = 0; i < strTok.length(); i++) {
+            if (strTok.charAt(i) == "-") {
+
+            }
+        }
+    }
+}
