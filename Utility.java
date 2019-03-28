6eb0062edb6b1fac072be6ae8941474290d1059b
celloguy <will_wright24@yahoo.com>
2019-03-28 13:08:46 -0500

    Created basic Utility class

---
+++ Utility.java
@@ -0,0 +1,24 @@
+package meatbol;
+
+public class Utility {
+    public ResultValue subtract(Parser parser, ResultValue rv1, ResultValue rv2){
+       ResultValue result = new ResultValue();
+
+       return result;
+    }
+    public ResultValue add(Parser parser, ResultValue rv1, ResultValue rv2) {
+        ResultValue result = new ResultValue();
+
+        return result;
+    }
+    public ResultValue multiply(Parser parser, ResultValue rv1, ResultValue rv2) {
+        ResultValue result = new ResultValue();
+
+        return result;
+    }
+    public ResultValue divide(Parser parser, ResultValue rv1, ResultValue rv2) {
+        ResultValue result = new ResultValue();
+
+        return result;
+    }
+}
