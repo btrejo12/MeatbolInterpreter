4ffdd3a99d45465e4e14f4d41c4559dc44d75726
celloguy <will_wright24@yahoo.com>
2019-04-13 20:46:02 -0500

    cleaned up expression a little, added Arrayz, and update resultValue

---
+++ Arrayz.java
@@ -0,0 +1,32 @@
+package meatbol;
+
+import java.util.ArrayList;
+
+public class Arrayz {
+    public int bounds;
+    //public String name;
+    private String arr[];
+
+
+    public Arrayz(int bounds){
+        //this.name = name;
+        this.bounds = bounds;
+        this.arr = new String[bounds];
+    }
+
+    public void add(int index, String value) throws Exception{
+        if(index >= bounds || index < 0){
+            throw new Exception();
+        }
+        arr[index] = value;
+    }
+
+    public void add(String value) throws Exception{
+        for(int i = 0; i< bounds; i++){
+            if(arr[i] != null)
+                arr[i] = value;
+        }
+
+    }
+
+}
