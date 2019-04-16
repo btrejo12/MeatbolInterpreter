1f0705ba1d7a1afc0285e3c708ed6cba018f4f4c
btrejo12 <trejo.brenda95@gmail.com>
2019-04-16 01:32:07 -0500

    Changed array data type back to array[] instead of array list

--- Arrayz.java
+++ Arrayz.java
@@ -3,9 +3,9 @@ package meatbol;
 import java.util.ArrayList;
 
 public class Arrayz {
-    public int bounds;
+    private int bounds;
     //public String name;
-    public ArrayList<ResultValue> arr = new ArrayList<ResultValue>();
+    public ResultValue []  arr;
     private ResultValue owner;
 
 
@@ -13,15 +13,49 @@ public class Arrayz {
         this.owner = owner;
     }
 
+    public void setBounds(ResultValue limit) throws Exception{
+        if (limit.type != SubClassif.INTEGER)
+            throw new Exception("Array size must be of type integer, found:" + limit.value);
+        arr = new ResultValue[Integer.parseInt(limit.value)];
+    }
+
     public void update(int index, ResultValue addition) throws Exception{
         if(index >= bounds || index < 0){
             throw new Exception("Array index out of bounds");
         }
-        arr.set(index, addition);
+        arr[index] = addition;
+        //TODO: Update the RV's value string in StorageManager.
     }
 
     public void add(ResultValue value) throws Exception{
-        arr.add(value);
+        // ???
+        //TODO: Update the RV's value string in StorageManager
+    }
+
+    /**
+     * Returns the subscript of the highest populated element
+     * @return The index of the highest populated element
+     */
+    public int elem(){
+        int ret = -1;
+        for(int i = arr.length; i >=0; i--){
+            if(arr[i] != null)
+                ret = i;
+        }
+        return ret;
+    }
+
+    /**
+     * Returns the number of populated elements.
+     * @return
+     */
+    public int maxelem(){
+        int counter = 0;
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] != null)
+                counter++;
+        }
+        return counter;
     }
 
     public String toString(){
